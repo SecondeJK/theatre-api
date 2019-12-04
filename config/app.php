4a58c161f5789f4852c702971cc02ff6c3161c89
Jim Seconde <jim.seconde@googlemail.com>
2019-12-04 08:28:34 +0000

    add service provider

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Vyuldashev\LaravelOpenApi\OpenApiServiceProvider;
+
 return [
 
     /*
@@ -161,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        OpenApiServiceProvider::class,
 
         /*
          * Package Service Providers...
