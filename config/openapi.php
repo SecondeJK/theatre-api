385100ddeaa2d67b9148e89298e35c22199c433b
Jim Seconde <jim.seconde@googlemail.com>
2019-12-04 08:27:58 +0000

    added openapi schema generator

---
+++ config/openapi.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    'collections' => [
+
+        'default' => [
+
+            'info' => [
+                'title' => config('app.name'),
+                'description' => null,
+                'version' => '1.0.0',
+            ],
+
+            'servers' => [
+                [
+                    'url' => env('APP_URL'),
+                ],
+            ],
+
+            'tags' => [
+
+                // [
+                //    'name' => 'user',
+                //    'description' => 'Application users',
+                // ],
+
+            ],
+
+            'security' => [
+                // GoldSpecDigital\ObjectOrientedOAS\Objects\SecurityRequirement::create()->securityScheme('JWT'),
+            ],
+
+            // Route for exposing specification.
+            // Leave uri null to disable.
+            'route' => [
+                'uri' => '/openapi',
+                'middleware' => [],
+            ],
+
+        ],
+
+    ],
+
+];
